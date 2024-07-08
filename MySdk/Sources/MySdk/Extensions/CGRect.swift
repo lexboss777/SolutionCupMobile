075156322fee3900eebb7bd8161626717fa0c1fb
ilnur.shafigullin <ilnur.shafigullin@tatar.ru>
2024-07-08 15:42:03 +0300

    public modifier is redundant

--- MySdk/Sources/MySdk/Extensions/CGRect.swift
+++ MySdk/Sources/MySdk/Extensions/CGRect.swift
@@ -9,11 +9,11 @@ import Foundation
 import UIKit
 
 public extension CGRect {
-    public init(x: CGFloat, y: CGFloat, size: CGSize) {
+    init(x: CGFloat, y: CGFloat, size: CGSize) {
         self.init(x: x, y: y, width: size.width, height: size.height)
     }
 
-    public init(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) {
+    init(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) {
         self.init(x: x, y: y, width: width, height: height)
     }
 }
